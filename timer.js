d067116d57b4fe5b11e360c65a6ae1ba1186c176
techeditorsrc <71694483+techeditorsrc@users.noreply.github.com>
2022-01-12 12:14:12 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,44 @@
+//Author: Anton Nedilko
+function timer(){
+  var me=this;
+  this.data=[];
+  this.index=0;
+  this.default_delay=25;
+  this.delay=0;
+  this.istimer=false;
+  this.timer=undefined;
+  
+  
+  this.update=function(){
+    if(this.istimer){
+      this.index+=1;
+    }
+  }
+  
+  this.run=function(x){
+    if(!x){
+      this.delay=this.default_delay;  
+    }else{
+      this.delay=x;
+    }
+    this.index=0;
+    this.istimer=true;
+    this.timer=setInterval(function(){
+      me.update();
+    },this.delay);
+  }
+  
+  this.pause=function(){
+    if(this.istimer){
+      this.istimer=false; 
+    }
+  }
+  
+  this.continue=function(){
+    if(!this.istimer){
+      this.istimer=true; 
+    }
+  }
+  
+}
 
