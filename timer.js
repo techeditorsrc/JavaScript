1319d11efa140f5edc406a01d986a0b26a3861b4
techeditorsrc <71694483+techeditorsrc@users.noreply.github.com>
2022-01-12 12:39:16 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -8,11 +8,40 @@ function timer(){
   this.istimer=false;
   this.timer=undefined;
   
+  //{"id":"","f":function(index){return true},"call":function(index){}}
+  this.register=function(x){
+    this.data.push(x);
+  }
   
-  this.update=function(){
-    if(this.istimer){
-      this.index+=1;
+  this.call_register=function(){
+    for(var x in this.data){
+      if(this.data[x].f(this.index)){
+        this.data[x].call(this.index);  
+      }
     }
+  }
+  
+  this.find_register=function(id){
+     result=-1;
+    for(var x in this.data){
+      if(this.data[x].id==id){
+        result=x;
+        break;
+      }
+    }
+    return result;
+  }
+  
+  this.delete_register(id){
+    var r=this.find_register(id);
+    while(r!=-1){
+      this.data.splice(r,1);
+      r=this.find_register(id);
+    }
+  }
+  
+  this.update=function(index){
+    this.call_register();
   }
   
   this.run=function(x){
@@ -24,7 +53,13 @@ function timer(){
     this.index=0;
     this.istimer=true;
     this.timer=setInterval(function(){
-      me.update();
+      if(this.istimer){
+        me.update(me.index);
+        me.index+=1;
+        if(me.index>=65536){
+          me.index=0; 
+        }
+      }
     },this.delay);
   }
   
@@ -37,6 +72,14 @@ function timer(){
   this.continue=function(){
     if(!this.istimer){
       this.istimer=true; 
+    }
+  }
+  
+  this.stop=function(){
+    if(this.istimer){
+      this.istimer=false;
+      clearInterval(me.timer);
+      this.index=0;
     }
   }
   
