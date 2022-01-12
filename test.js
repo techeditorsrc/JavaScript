8fff786f5a6f82ad4261bf3520cafe840cf7b4bb
techeditorsrc <71694483+techeditorsrc@users.noreply.github.com>
2022-01-12 12:01:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,39 @@
 // Author: Anton Nedilko
 
-function get(id){
-  return document.getElementById(id); 
-}
+// function get(id){
+//   return document.getElementById(id); 
+// }
 
-function html(id,text,append){
-  if(!append){
-    id.innerHTML=text; 
-  }else{
-    id.innerHTML+=text; 
-  }
-}
+// function html(id,text,append){
+//   if(!append){
+//     id.innerHTML=text; 
+//   }else{
+//     id.innerHTML+=text; 
+//   }
+// }
 
-var c_id;
-function init_console(id){
-  c_id=get(id); 
-}
-con=function(text,append){
-  if(id){
-    html(c_id,text,append);
-  }
-}
+// var c_id;
+// function init_console(id){
+//   c_id=get(id); 
+// }
+// con=function(text,append){
+//   if(id){
+//     html(c_id,text,append);
+//   }
+// }
 
-function timerobj(){
-  this.c=0;
-  this.x="";
-  var me=this;
-  this.update=function(){
-    con(me.c);
-    this.c+=1;
-  }
-  this.set=function(){
-    this.x=setInterval(function(){
-      me.update();
-    },123);
-  }
-}
+// function timerobj(){
+//   this.c=0;
+//   this.x="";
+//   var me=this;
+//   this.update=function(){
+//     con(me.c);
+//     this.c+=1;
+//   }
+//   this.set=function(){
+//     this.x=setInterval(function(){
+//       me.update();
+//     },123);
+//   }
+// }
 
