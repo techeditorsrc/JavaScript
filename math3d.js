821150088ba2ef15d2a8346515964127dac0d836
techeditorsrc <71694483+techeditorsrc@users.noreply.github.com>
2022-01-23 14:06:06 +0200

    Update math3d.js

--- math3d.js
+++ math3d.js
@@ -1,1 +1,70 @@
+<style>
+</style>
 
+<canvas width="128" height="128" style="border:solid thin;display:block;float:left">
+</canvas>
+<div id="console" style="display:block;">
+  
+</div>
+<script>
+  function Vector3d(){
+    this.vector=function(){
+      var x=arguments,l=arguments.length;
+      if(l==0){
+        this.v=[0,0,0,0]
+      }else if(l==1){
+        if(Array.isArray(x[0])){
+          this.v=[x[0][0],x[0][1],x[0][2],x[0][3]];
+        }else{
+          this.v=[x[0].v[0],x[0].v[1],x[0].v[2],x[0].v[3]];
+        }
+      }else if(l==4){
+        this.v=[x[0],x[1],x[2],x[3]];
+      }
+    }
+    this.v=[];
+    this.vector(arguments);
+    this.add=function(a,b){
+      var x=[0,0,0,0],y=[0,0,0,0];
+      if(a){
+        if(Array.isArray(a)){
+          x=a;
+        }else{
+          x=a.v;
+        }
+        if(!b){
+          for(var j=0;j<3;j++){
+            this.v[j]+=x[j];
+          }
+          return this;
+        }else{
+          if(Array.isArray(b)){
+            y=b;
+          }else{
+            y=b.v;
+          }
+          var result=new vector3d(x);
+          return result.add(y);
+        }
+      }else{
+        var result=new vector3d();
+        return result;
+      }       
+    }
+  }
+  
+  function Matrix3d(){
+    
+  }
+  function get(id){
+    return document.getElementById(id);
+  }
+  var console=get("console")
+  function print(x,y){
+    if(!y){
+      console.innerHTML=x;
+    }else{
+      console.innerHTML+=x;
+    }
+  }
+</script>
